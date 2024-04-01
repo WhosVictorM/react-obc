2bb9cb8e1db9a4367310806f11ecea3887dd6d23
WhosVictorM <victormagalhaespb@gmail.com>
2024-04-01 10:13:04 -0300

    08-estilizando-componentes

---
+++ 08-estilizando-componentes/src/components/Subtitle.jsx
@@ -0,0 +1,8 @@
+export const Subtitle = () => (
+<h2
+    style={ {
+        color: "#e5e5e5"
+    } }
+>
+    It's easy, like 1 + 1 = {1 + 1}</h2>
+)
---
+++ 08-estilizando-componentes/src/components/Title/index.jsx
@@ -0,0 +1,4 @@
+export default function Title() {
+    const tech = "React"
+    return <h1 style={{ color: "#fff" }}>{tech} is Awesome!</h1>
+  }
