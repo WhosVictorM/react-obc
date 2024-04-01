4e125e656d52916b02db11e0754ee58a099408de
WhosVictorM <victormagalhaespb@gmail.com>
2024-04-01 09:36:15 -0300

    07-criando-componentes

---
+++ 07-criando-componentes/src/App.jsx
@@ -0,0 +1,17 @@
+import Title from "./components/Title"
+import { Subtitle } from "./components/Subtitle"
+import StatusText from "./components/StatusText"
+
+function sum(a,b){
+  return a + b
+}
+
+export default function App() {
+  return (
+    <div>
+      <Title/>
+      <Subtitle/>
+      <StatusText/>
+    </div>
+  )
+}
